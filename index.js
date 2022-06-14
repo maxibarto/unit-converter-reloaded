1c89d32272ecbfeb2c00f2536f77145a150096f9
Maxi Bartolozzi <maxibartolozzi@gmail.com>
2022-06-14 19:12:18 -0300

    initial const declared

---
+++ index.js
@@ -0,0 +1,37 @@
+const insValue = document.getElementById('ins-value').value
+const convertBtn = document.getElementById('convert-btn')
+const conversionList = document.getElementById("conversion-list")
+const magnitudes = {
+    length: {
+        mgtbase: "meter",
+        mgtsec: "feet",
+        ratio: 3.281
+    },
+    volume: {
+        mgtbase: "liter",
+        mgtsec: "gallon",
+        ratio: 0.264
+    },
+    mass: {
+        mgtbase: "kilogram",
+        mgtsec: "pound",
+        ratio: 2.204
+    }
+} 
+
+
+function convert(from, to){
+
+}
+
+
+
+
+
+
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
