0b0a603a80c50741c984175e4e011e134689f75f
Maxi Bartolozzi <maxibartolozzi@gmail.com>
2022-06-15 00:43:15 -0300

    convert function

--- index.js
+++ index.js
@@ -3,25 +3,45 @@ const convertBtn = document.getElementById('convert-btn')
 const conversionList = document.getElementById("conversion-list")
 const magnitudes = {
     length: {
-        mgtbase: "meter",
-        mgtsec: "feet",
+        mgtbase: "meters",
+        mgtsec: "feets",
         ratio: 3.281
     },
     volume: {
-        mgtbase: "liter",
-        mgtsec: "gallon",
+        mgtbase: "liters",
+        mgtsec: "gallons",
         ratio: 0.264
     },
     mass: {
-        mgtbase: "kilogram",
-        mgtsec: "pound",
+        mgtbase: "kilograms",
+        mgtsec: "pounds",
         ratio: 2.204
     }
 } 
 
 
-function convert(from, to){
+const magnCount = Object.keys(magnitudes)
+const magnTypes = Object.values(magnitudes)
 
+function convert(){
+    let results1 = []
+    let results2 = []
+    let magnList = []
+    for (let i = 0; i < magnCount.length; i++){
+        results1[i] = (insValue * magnTypes[i].ratio).toFixed(3)
+        results2[i] = (insValue / magnTypes[i].ratio).toFixed(3)
+
+        magnList[i] = `
+        <li>
+            <h3>${magnCount[i]} (${magnTypes[i].mgtbase} / ${magnTypes[i].mgtsec})</h3>
+            <p>
+                ${insValue} ${magnTypes[i].mgtbase} = ${results1[i]} ${magnTypes[i].mgtsec} | 
+                ${insValue} ${magnTypes[i].mgtsec} = ${results2[i]} ${magnTypes[i].mgtbase}
+            </p>       
+        </li>
+        `
+    }
+    conversionList.innerHTML = magnList
 }
 
 
